ec6112eae13023b6a47886401003934a12c7d536
harbar20 <harbar2021@gmail.com>
2020-07-28 11:42:47 -0400

    added conversion command

--- index.js
+++ index.js
@@ -738,6 +738,66 @@ bot.on("message", async (message) => {
 					"Porygon used Tri-Attack! No secondary effect on the target."
 				);
 		}
+	} else if (msgStr.toLowerCase().startsWith(`${prefix} conversion`)) {
+		let rand = Math.round(Math.random() * (17-0+1)+0);
+		let type = "";
+		switch (rand) {
+			case 0:
+				type = "Bug";
+				break;
+			case 1:
+				type = "Dark";
+				break;
+			case 2:
+				type = "Dragon";
+				break;
+			case 3:
+				type = "Electric";
+				break;
+			case 4:
+				type = "Fairy";
+				break;
+			case 5:
+				type = "Fighting";
+				break;
+			case 6:
+				type = "Fire";
+				break;
+			case 7:
+				type = "Flying";
+				break;
+			case 8:
+				type = "Ghost";
+				break;
+			case 9:
+				type = "Grass";
+				break;
+			case 10:
+				type = "Ground";
+				break;
+			case 11:
+				type = "Ice";
+				break;
+			case 12:
+				type = "Normal";
+				break;
+			case 13:
+				type = "Poison";
+				break;
+			case 14:
+				type = "Psychic";
+				break;
+			case 15:
+				type = "Rock";
+				break;
+			case 16:
+				type = "Steel";
+				break;
+			case 17:
+				type = "Water";
+				break;
+		}
+		return channel.send(`Porygon used Conversion! Porygon's type changed to ${type}!`);
 	}
 });
 
