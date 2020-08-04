abf4759524dec492532db70c0627ba9781f3c008
harbar20 <harbar2021@gmail.com>
2020-08-04 19:03:54 -0400

    removed test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const Client = require('ps-client').Client;
-let Bot = new Client({username: 'PorygonTesting', password: 'porygonisverycool', debug: true, avatar: 'supernerd'});
-
-Bot.connect();
-
-Bot.on("connect", () => {
-    console.log(Bot.send('/join botdevelopment'));
-})
