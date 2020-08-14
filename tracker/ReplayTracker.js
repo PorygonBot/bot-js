d1ff40dbc6d8107cdec6c0e487046e3228fa883b
harbar20 <harbar2021@gmail.com>
2020-08-13 23:01:58 -0400

    fixed zoroark in showdown.js as well

--- tracker/ReplayTracker.js
+++ tracker/ReplayTracker.js
@@ -491,17 +491,21 @@ class ReplayTracker {
 				//Mostly used for Illusion cuz frick Zoroark
 				else if (line.startsWith(`|-end|`)) {
 					let historyLine = battle.history[battle.history.length - 1];
-					if (line.endsWith("Illusion") && historyLine.includes(battle.turns.toString())) {
+					if (
+						line.endsWith("Illusion") &&
+						historyLine.includes(battle.turns.toString())
+					) {
 						let historyLineParts = historyLine.split(" ");
 						let victim = historyLineParts[0];
 						let killer = historyLineParts[4];
-						let isPassive = historyLineParts[historyLineParts.length - 2] === "(passive)";
-						
+						let isPassive =
+							historyLineParts[historyLineParts.length - 2] ===
+							"(passive)";
+
 						if (battle.p1Pokemon[victim]) {
 							battle.p1Pokemon[victim].undied();
 							battle.p2Pokemon[killer].unkilled(isPassive);
-						}
-						else {
+						} else {
 							battle.p2Pokemon[victim].undied();
 							battle.p1Pokemon[killer].unkilled(isPassive);
 						}
