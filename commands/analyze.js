96b0db9f690f496b0ccd9824d485f8ddff2d2881
harbar20 <harbar2021@gmail.com>
2020-11-25 18:52:57 -0500

    added multiple replay analysis feature

--- commands/analyze.js
+++ commands/analyze.js
@@ -8,19 +8,21 @@ module.exports =  {
     aliases: ['analyse'],
     async execute(message, args) {
         const channel = message.channel;
-        
-        let link = args[0] + ".log";
-		let response = await axios.get(link);
-		let data = response.data;
-
-		//Getting the rules
-		let rulesId = await utils.findRulesId(channel.id);
-		let rules = await utils.getRules(rulesId);
-		console.log(rules);
-
-		let replayer = new ReplayTracker(args[0], message, rules);
-		channel.send("Analyzing...");
-		await replayer.track(data);
-		console.log(`${link} has been analyzed!`);
+		
+		for (let arg of args) {
+			let link = arg + ".log";
+			let response = await axios.get(link);
+			let data = response.data;
+	
+			//Getting the rules
+			let rulesId = await utils.findRulesId(channel.id);
+			let rules = await utils.getRules(rulesId);
+			console.log(rules);
+	
+			let replayer = new ReplayTracker(args[0], message, rules);
+			channel.send("Analyzing...");
+			await replayer.track(data);
+			console.log(`${link} has been analyzed!`);
+		}
     }
 }
