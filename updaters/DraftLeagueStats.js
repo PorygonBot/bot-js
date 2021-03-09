5cfb1a999b0b8b567baec65a1ae05b8744f57bb3
harbar20 <harbar2021@gmail.com>
2021-03-09 15:41:25 -0500

    tests worked again!

--- updaters/DraftLeagueStats.js
+++ updaters/DraftLeagueStats.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
 const utils = require("../utils.js");
+const DiscordDefaultStats = require("../updaters/DiscordDefaultStats");
 
 class DraftLeagueStats {
 	constructor(message) {
@@ -11,12 +12,16 @@ class DraftLeagueStats {
 	}
 
 	async update(matchJson) {
+		let psPlayer1 = Object.keys(matchJson.players)[0];
+		let psPlayer2 = Object.keys(matchJson.players)[1];
+
 		try {
 			//Getting league data
 			const leagueResponse = await axios.get(
 				`${process.env.DL_API_URL}/league/${matchJson.league_id}?key=${process.env.DL_API_KEY}`
 			);
 			const leagueData = leagueResponse.data;
+			console.log("League recieved.");
 
 			//Getting the Discord user player from their Discord ID
 			const authorID = this.author.id;
@@ -37,16 +42,21 @@ class DraftLeagueStats {
 				discordUserPS === Object.keys(matchJson.players)[0]
 					? Object.keys(matchJson.players)[1]
 					: Object.keys(matchJson.players)[0];
+			console.log("Players recieved.");
 
 			//Getting the Match ID based on opponent's pokemon
-			const matchResponse = await axios.get(
-				`${process.env.DL_API_URL}/league/${
-					matchJson.league_id
-				}/player/<@${authorID}>?pokemon=${Object.keys(
-					matchJson.players[nonDiscordUserPS].kills
-				).join(",")}&key=${process.env.DL_API_KEY}`
-			);
+			const matchURL = `${process.env.DL_API_URL}/league/${
+				matchJson.league_id
+			}/player/<@${authorID}>?pokemon=${Object.keys(
+				matchJson.players[nonDiscordUserPS].kills
+			)
+				.join(",")
+				.replace("’", "")}&key=${process.env.DL_API_KEY}`;
+			console.log(matchURL);
+			const matchResponse = await axios.get(matchURL);
 			const matchData = matchResponse.data;
+			console.log("Match recieved.");
+			console.log(matchData);
 
 			matchJson.players[discordUserPS].league_id = discordPlayerData.id;
 			matchJson.players[nonDiscordUserPS].league_id = matchData.opponent;
@@ -62,22 +72,30 @@ class DraftLeagueStats {
 				`${process.env.DL_API_URL}/submission?key=${process.env.DL_API_KEY}`,
 				final
 			);
+			console.log("Submitted");
 
 			//Posting to the replay webhook
-			let result = matchJson.info.result;
-			result = result.startsWith(discordUserPS)
-				? result.substring(result.length - 3)
-				: `${result.substring(result.length - 1)}-${result.substring(
-						result.length - 3,
-						result.length - 2
-				  )}}}`;
+			let result = matchJson.info.result.toLowerCase().startsWith(discordUserPS)
+				? matchJson.info.result.substring(
+						matchJson.info.result.length - 3
+				  )
+				: `${matchJson.info.result.substring(
+						matchJson.info.result.length - 1
+				  )}-${matchJson.info.result.substring(
+						matchJson.info.result.length - 3,
+						matchJson.info.result.length - 2
+				  )}`;
 			await axios.post(leagueData.replay_webhook, {
-				content: `A match in the ${leagueData.league_name} between the ${discordPlayerData.team_name} and the ${matchData.opponent_team_name} has just been submitted *by Porygon*.\nReplay: ${matchJson.info.replay}\n**History:** ${matchJson.info.history}\nResult: ||${result}||`,
+				content: `A match in the ${leagueData.league_name} between the ${discordPlayerData.team_name} and the ${matchData.opponent_team_name} has just been submitted by Porygon Automatic Import.\nReplay: <${matchJson.info.replay}>\nResult: ||${result}||`,
 			});
+			await this.channel.send(
+				`Battle between \`${psPlayer1}\` and \`${psPlayer2}\` is complete and info has been updated!`
+			);
 		} catch (e) {
 			await this.channel.send(
 				":x: There was an error updating this match. Please paste these stats instead: "
 			);
+			console.error(e);
 			//Send the stats
 			let defaulter = new DiscordDefaultStats(this.message);
 			await defaulter.update(matchJson);
--- utils.js
+++ utils.js
@@ -599,7 +599,6 @@ const randomElement = (list) => {
 // include the elements in the second
 // array then return true.
 function findCommonElements(arr1, arr2) {
-	console.log("Arr1: " + arr1 + "\nArr2: " + arr2);
 	return arr1.some((item) => arr2.includes(item));
 }
 
