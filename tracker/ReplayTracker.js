03cd3ee13b2613bca60ddf9dd59f4f74da3c8fc4
harbar20 <harbar2021@gmail.com>
2021-04-06 21:39:30 -0400

    in -end, checks if history line is about kills and not something else

--- tracker/ReplayTracker.js
+++ tracker/ReplayTracker.js
@@ -1087,16 +1087,16 @@ class ReplayTracker {
 				//Mostly used for Illusion cuz frick Zoroark
 				else if (line.startsWith(`|-end|`)) {
 					let historyLine =
-						battle.history[battle.history.length - 1] || "";
+						battle.history.filter((line) => line.includes(" was killed by "))[battle.history.length - 1] || "";
 					if (
 						line.endsWith("Illusion") &&
 						historyLine.includes(battle.turns.toString())
 					) {
-						let historyLineParts = historyLine.split(" ");
+                        let historyLineParts = historyLine.split(" ");
 						let victim = historyLine.split(" was killed by ")[0];
 						let killer = historyLine
 							.split(" was killed by ")[1]
-							.split(" due to ")[0];
+                            .split(" due to ")[0];
 						let isPassive =
 							historyLineParts[historyLineParts.length - 2] ===
 							"(passive)";
