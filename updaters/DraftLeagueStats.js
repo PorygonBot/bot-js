d3a739f9b9c6bef0f33f15fa923e01d239c1b1d3
harbar20 <harbar2021@gmail.com>
2021-06-11 11:45:36 -0400

    fixed error message in dl updater

--- updaters/DraftLeagueStats.js
+++ updaters/DraftLeagueStats.js
@@ -118,7 +118,7 @@ class DraftLeagueStats {
 				`:x: Error with match number \`${
 					matchJson.battleId
 				}\`. I will be unable to analyze this match until you screenshot this message and send it to the Porygon server's bugs-and-help channel and ping harbar20 in the same channel.\n\n**Error:**\`\`\`${
-					e.response.data
+					JSON.stringify(e.response.data)
 				}\nLine number: ${
 					e.stack.split(":")[2]
 				}\`\`\`\nPlease paste these stats instead: `
